4b9d88db2db6874464379ff2acedac514c720ad5
DongOk <hcan159@naver.com>
2023-12-24 23:30:01 +0900

    @add fc

---
+++ src/states/states.ts
@@ -0,0 +1,10 @@
+import PropTypes from 'prop-types';
+import React, {Component} from 'react';
+
+export default class states extends Component {
+  static propTypes = {second: third};
+
+  render() {
+    return <div>states</div>;
+  }
+}
